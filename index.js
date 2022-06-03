e526306afc79a7eeb46573266ad843981dc4a352
timotheus ingga pranata <trancexe@gmail.com>
2022-06-04 00:42:12 +0900

    update index.js 152235865101-246 timotheus ingga pranata
    152235865101-246 timotheus ingga pranata
    
    feature: sortAscending, sortDescending, sorter. number ditambahkan ke array pada function sorter

--- index.js
+++ index.js
@@ -6,16 +6,28 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (names,sort) => {
+  let fun = sort(names);
+  nameOfString = [];
+  for (let counter = 0; counter < fun.length; counter++) {
+    nameOfString.push((counter+1) + ". " + fun[counter]);
+  }
+  return nameOfString;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending= (names) => {
+  return names.sort();
+};
+console.log(sortAscending(names));
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
-
+const sortDescending = (names) => {
+  return names.sort().reverse();
+};
+console.log(sortDescending(names));
 // ! JANGAN DIMODIFIKASI
 (function main() {
   console.log(sorter?.(names, sortAscending)?.join("\n"));
